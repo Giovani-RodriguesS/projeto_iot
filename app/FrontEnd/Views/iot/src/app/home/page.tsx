f2ddd482b941a96f5a05cdf75c1ce00a4e486f3c
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-08 19:50:35 -0300

    feat:add new home page

---
+++ app/FrontEnd/Views/iot/src/app/home/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'; 
+import { TabView, TabPanel } from 'primereact/tabview';
+import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
+import '../globals.css';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="flex-1 bg-black">
+        <Header title= "Irrigação Smart" username="Usuário"/>
+        <Navbar />
+        <main className='custom-tabview flex justify-center min-h-screen mt-8'>
+            <div className="card bg-gray-700 p-6 rounded-lg shadow mt-0 text-3xl w-11/12">
+                 <TabView className='margin-left: auto'>
+                    <TabPanel header="Sobre o Projeto" leftIcon="pi pi-file-edit">  
+                        <p className="m-0 text-gray-300 leading-relaxed text-justify"style={{ textIndent: '3rem' }}>
+                        O projeto envolve duas turmas de tecnólogos, Análise e Desenvolvimento de Sistemas (ADS) e Mecatrônica, que estão criando um sistema automatizado para regar uma planta.
+                        O sistema utilizará sensores de umidade no solo e de chuva para determinar a necessidade de irrigação. A turma de ADS desenvolverá um site para receber e exibir os dados dos sensores, enquanto a turma de Mecatrônica ficará responsável pela implementação do sistema de automação, incluindo a bomba de irrigação e os sensores.
+                        </p>
+                    </TabPanel>
+                    <TabPanel header="Turma Mecatrônica" leftIcon="pi pi-wrench">
+                        <p className="m-0 text-gray-300 leading-relaxed text-justify"style={{ textIndent: '3rem' }}>
+                        A turma de Mecatrônica será responsável pela parte de hardware do projeto, que envolve a instalação e integração dos sensores de umidade do solo e de chuva.
+                        Eles também irão configurar o sistema de automação para ativar a bomba de irrigação quando necessário, com base nas leituras dos sensores. A equipe se concentrará no desenvolvimento e controle dos dispositivos eletrônicos, garantindo a funcionalidade do sistema.
+                        </p>
+                    </TabPanel>
+                    <TabPanel header="Turma ADS" leftIcon="pi pi-desktop">
+                        <p className="m-0 text-gray-300 leading-relaxed text-justify"style={{ textIndent: '3rem' }}>
+                        A turma de ADS ficará encarregada do desenvolvimento do site, que será responsável por receber, processar e exibir os dados dos sensores de umidade do solo e de chuva.
+                        Eles trabalharão na criação da interface do usuário, implementando funcionalidades como visualização de gráficos e histórico de dados, e garantirão a comunicação eficaz entre o site e o sistema de sensores.
+                        </p>
+                    </TabPanel>
+                </TabView>
+            </div>
+        </main>
+      </div>
+    </div>
+  );
+}
