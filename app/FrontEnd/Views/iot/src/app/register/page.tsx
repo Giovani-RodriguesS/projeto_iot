35344dc6de450884fbbc14628790ec3170be3bda
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-12 22:20:24 -0300

    feature:rename variables and add job

--- app/FrontEnd/Views/iot/src/app/register/page.tsx
+++ app/FrontEnd/Views/iot/src/app/register/page.tsx
@@ -7,22 +7,24 @@ import { useRouter } from 'next/navigation';
 import axios from "axios";
 
 export default function StyledMaskDemo() {
-    const [name, setname] = useState('');
+    const [nome, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [cargo, setCargo] = useState('');
+    const [telefone, setPhone] = useState('');
+    const [senha, setPassword] = useState('');
+    const [senhaVisible, setPasswordVisible] = useState(false);
     const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://localhost:3000/register', {
-                name,
+            const response = await axios.post('http://localhost:5257/api/', {
+                nome,
                 email,
-                phone,
-                password,
+                cargo,
+                telefone,
+                senha,
             });
 
             if (response.status === 200) {
@@ -50,7 +52,8 @@ export default function StyledMaskDemo() {
                             <input
                                 type="text"
                                 id="nome"
-                                name="nome"
+                                value={nome}
+                                onChange={(e) => setName(e.target.value)}
                                 required
                                 className="w-full p-3 bg-slate-800 text-white rounded-md"
                                 placeholder="Digite seu nome"
@@ -63,22 +66,39 @@ export default function StyledMaskDemo() {
                             <input
                                 type="email"
                                 id="email"
-                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 required
                                 className="w-full p-3 bg-slate-800 text-white rounded-md"
                                 placeholder="Digite seu email"
                             />
                         </div>
 
+                        {/* Campo Cargo */}
+                        <div>
+                            <label htmlFor="cargo" className="text-gray-400 block mb-2">Cargo</label>
+                            <input
+                                type="text"
+                                id="cargo"
+                                value={cargo}
+                                onChange={(e) => setCargo(e.target.value)}
+                                required
+                                className="w-full p-3 bg-slate-800 text-white rounded-md"
+                                placeholder="Digite seu cargo"
+                            />
+                        </div>
+
                         {/* Campo Telefone */}
                         <div>
-                            <label htmlFor="phone" className="text-gray-400 block mb-2">Telefone</label>
-                            <InputMask
-                                id="phone"
-                                mask="(99) 99999-9999"
-                                placeholder="(99) 99999-9999"
-                                className="w-full p-3 bg-slate-800 text-white rounded-md"
-                            />
+                        <label htmlFor="cargo" className="text-gray-400 block mb-2">Telefone</label>
+                        <InputMask
+                            id="phone"
+                            value={telefone}
+                            onChange={(e) => setPhone(e.value as string)}
+                            mask="(99) 99999-9999"
+                            placeholder="(99) 99999-9999"
+                            className="w-full p-3 bg-slate-800 text-white rounded-md"
+                        />
                         </div>
 
                         {/* Campo Senha */}
@@ -86,19 +106,18 @@ export default function StyledMaskDemo() {
                             <label htmlFor="password" className="text-gray-400 block mb-2">Senha</label>
                             <div className="relative w-full">
                                 <input
-                                    type={passwordVisible ? "text" : "password"}
+                                    type={senhaVisible ? "text" : "password"}
                                     id="password"
-                                    value={password}
+                                    value={senha}
                                     onChange={(e) => setPassword(e.target.value)}
                                     className="w-full p-3 bg-slate-800 text-white rounded-md pr-10"
                                     placeholder="Digite sua senha"
                                 />
-                                {/* Ícone de Olho no final do campo */}
                                 <span
-                                    onClick={() => setPasswordVisible(!passwordVisible)}
+                                    onClick={() => setPasswordVisible(!senhaVisible)}
                                     className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
                                 >
-                                    <i className={`pi ${passwordVisible ? 'pi-eye-slash' : 'pi-eye'}`} />
+                                    <i className={`pi ${senhaVisible ? 'pi-eye-slash' : 'pi-eye'}`} />
                                 </span>
                             </div>
                         </div>
