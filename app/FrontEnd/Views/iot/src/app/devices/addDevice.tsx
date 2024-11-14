a844ca9f2f7cfcc982bffc5edbaa5d3bd3d87b12
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-14 18:46:55 -0300

    feat:add page to add device

--- app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
@@ -2,20 +2,23 @@
 import React, { useState } from 'react';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
+import SidebarDevices from './sidebar';
 
-export default function addDevice() {
+export default function AddDevice() {
     const [visible, setVisible] = useState<boolean>(false);
 
     return (
-        <div className="card flex justify-content-center">
-            <Sidebar visible={visible} onHide={() => setVisible(false)}>
-                <h2>Sidebar</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
+        <div className="card flex justify-center">
+            <Sidebar 
+                visible={visible} 
+                onHide={() => setVisible(false)} 
+                style={{ top: '10%' }} 
+                className="w-full md:w-80 h-3/4"
+            >
+                <h2>Cadastro de Dispositivo</h2>
+                <SidebarDevices/>
             </Sidebar>
-            <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
+            <Button icon="pi pi-file-plus" onClick={() => setVisible(true)} />
         </div>
     )
 }
