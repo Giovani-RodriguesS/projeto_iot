71052e1e93891df533c78c2697781f97b31290fc
Henferper <henriquealcalde@hotmail.com>
2024-11-14 11:26:06 -0300

    feat:change chart

--- app/FrontEnd/Views/iot/src/app/dashboard/LineChart.tsx
+++ app/FrontEnd/Views/iot/src/app/dashboard/LineChart.tsx
@@ -1,33 +1,63 @@
-'use client';
-import React, { useState, useEffect } from "react";
-import { Chart } from "primereact/chart";
+'use client'
+import React, { useState, useEffect } from 'react';
+import { Chart } from 'primereact/chart';
 
 export default function LineChart() {
     const [chartData, setChartData] = useState({});
     const [chartOptions, setChartOptions] = useState({});
 
     useEffect(() => {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
         const data = {
-            labels: ['Jan', 'Febr', 'Mar', 'Abri', 'Mai', 'Jun', 'Jul'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
-                    label: 'Produção',
-                    data: [70, 68, 80, 81, 56, 55, 60],
-                    fill: true,
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
                     tension: 0.4
                 },
                 {
-                    label: 'Defeitos',
-                    data: [4, 5, 2, 7, 1, 2, 3],
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
+                    borderColor: documentStyle.getPropertyValue('--pink-500'),
                     tension: 0.4
                 }
             ]
         };
-
         const options = {
             maintainAspectRatio: false,
-            aspectRatio: 1,
+            aspectRatio: 0.6,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                }
+            }
         };
 
         setChartData(data);
@@ -36,7 +66,7 @@ export default function LineChart() {
 
     return (
         <div className="card">
-            <Chart type="bar" data={chartData} options={chartOptions} className=""/>
+            <Chart type="line" data={chartData} options={chartOptions} />
         </div>
-    );
+    )
 }
