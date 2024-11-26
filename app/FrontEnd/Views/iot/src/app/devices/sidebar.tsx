4a74bde2a615845448b065c75fd9853fdb1a7070
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-26 16:48:09 -0300

    feat:change input and rename the placeholder

--- app/FrontEnd/Views/iot/src/app/devices/sidebar.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/sidebar.tsx
@@ -10,31 +10,31 @@ export default function SidebarDevices() {
                 <div className="text-blue-500 text-2xl mr-4">
                     <i className="pi pi-cog"></i>
                 </div>
-                <InputText placeholder="Nome" className="w-full p-2 focus:outline-none" />
+                <InputText placeholder="Bomba  /  Sensor" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
             </div>
 
             <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
                 <div className="text-blue-500 text-2xl mr-4">
                     <i className="pi pi-code"></i>
                 </div>
-                <InputNumber placeholder="ID" className="w-full p-2 focus:outline-none" />
+                <InputText placeholder="ID  ou  descrição" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
             </div>
 
             <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
                 <div className="text-blue-500 text-2xl mr-4">
                     <i className="pi pi-info-circle"></i>
                 </div>
-                <InputText placeholder="Tipo" className="w-full p-2 focus:outline-none" />
+                <InputText placeholder="Tipo" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
             </div>
 
             <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
                 <div className="text-blue-500 text-2xl mr-4">
                     <i className="pi pi-sliders-v"></i>
                 </div>
-                <InputText placeholder="Planta" className="w-full p-2 focus:outline-none" />
+                <InputText placeholder="Planta" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
             </div>
 
-                <Button label="Submit" />
+                <Button label="Cadastrar" style={{color:'black', borderColor: 'black'}}/>
         </div>
     );
 }
