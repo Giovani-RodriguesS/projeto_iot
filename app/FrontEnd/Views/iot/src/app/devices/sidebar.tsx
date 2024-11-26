8ae7f64352f94ff190d4b36323b8eb10b55a3079
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-26 19:41:37 -0300

    feat:add new page to add device

--- app/FrontEnd/Views/iot/src/app/devices/sidebar.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/sidebar.tsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
-import { Button } from 'primereact/button';
+import React, { useState } from "react";
+import { SelectButton, SelectButtonChangeEvent } from "primereact/selectbutton";
+import Water_pump from "./type/water_pump";
+import Sensor from "./type/sensor";
 
 export default function SidebarDevices() {
-    return (
-        <div className="card flex flex-col gap-4 p-6 bg-gray-100 rounded-lg shadow-lg">
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-cog"></i>
-                </div>
-                <InputText placeholder="Bomba  /  Sensor" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
+  const options: { label: string; value: string }[] = [
+    { label: "Bomba", value: "Bomba" },
+    { label: "Sensor", value: "Sensor" },
+  ];
 
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-code"></i>
-                </div>
-                <InputText placeholder="ID  ou  descrição" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
+  const [value, setValue] = useState<string>(options[0].value);
 
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-info-circle"></i>
-                </div>
-                <InputText placeholder="Tipo" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
+  return (
+    <div className="card flex flex-col bg-gray-100 rounded-lg mt-4">
+      {/* Botão de seleção */}
+      <div className="flex justify-center mb-1">
+        <SelectButton
+          value={value}
+          onChange={(e: SelectButtonChangeEvent) => setValue(e.value)}
+          options={options}
+          optionLabel="label"
+          optionValue="value"
+          className="w-full p-button-outlined"
+          itemTemplate={(option) => (
+            <div
+              className={`p-3 rounded-lg cursor-pointer ${
+                value === option.value ? "bg-blue-500 text-white" : "bg-gray-200 text-black"
+              }`}
+            >
+              {option.label}
             </div>
+          )}
+        />
+      </div>
 
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-sliders-v"></i>
-                </div>
-                <InputText placeholder="Planta" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
-
-                <Button label="Cadastrar" style={{color:'black', borderColor: 'black'}}/>
-        </div>
-    );
+      {/* Renderizar o componente baseado na seleção */}
+      <div className="">
+        {value === "Bomba" ? <Water_pump /> : <Sensor />}
+      </div>
+    </div>
+  );
 }
