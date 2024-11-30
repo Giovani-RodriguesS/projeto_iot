e64fad2c07b705fed5c3bb077f124922b14c15a3
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-29 21:14:49 -0300

    feat:add sensor

--- app/FrontEnd/Views/iot/src/app/devices/type/sensor.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/type/sensor.tsx
@@ -4,7 +4,7 @@ import { InputText } from "primereact/inputtext";
 
 export default function Sensor() {
     return (
-        <div className="card flex flex-col gap-4 p-6 bg-gray-100 rounded-lg">
+        <div className="card flex flex-col gap-4 p-3 bg-gray-100 rounded-lg">
             <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
                 <div className="text-blue-500 text-2xl mr-4">
                     <i className="pi pi-cog"></i>
@@ -25,20 +25,6 @@ export default function Sensor() {
                 </div>
                 <InputText placeholder="Data Instalação" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
             </div>
-            <Button 
-                label="Cadastrar" 
-                style={{
-                    color: 'white', 
-                    backgroundColor: '#3b82f6', 
-                    padding: '10px 20px',
-                    borderWidth: '2px',
-                    borderRadius: '8px',
-                    fontSize: '1rem',
-                    fontWeight: 'bold',
-                    transition: 'all 0.3s ease'
-                }} 
-                className="hover:bg-gray-200"
-            />
         </div>
     );
 }
