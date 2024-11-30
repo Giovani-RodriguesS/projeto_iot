0c5fc4d4f84ef63c87560746a1deb4143ab958f7
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-29 21:17:32 -0300

    feat:add device

--- app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
-import SidebarDevices from './sidebar';
+import SidebarDevices from './SidebarDevices';
 
 export default function AddDevice() {
     const [visible, setVisible] = useState<boolean>(false);
@@ -13,7 +13,7 @@ export default function AddDevice() {
                 visible={visible} 
                 onHide={() => setVisible(false)}
                 style={{ top: '10%' }}
-                className="w-full md:w-80 h-3/4 text-center bg-transparent justify-center sidebar-large-text">
+                className="w-full md:w-80 h-4/5 text-center bg-transparent justify-center sidebar-large-text">
                 <h2 className="sidebar-title mb-2" style={{fontSize:'1.5rem'}}>Cadastro Dispositivo</h2>
                 <SidebarDevices />
             </Sidebar>
