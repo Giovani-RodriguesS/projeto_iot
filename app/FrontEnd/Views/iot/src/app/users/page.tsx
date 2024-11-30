e19045bbc622317055eff4804123f99e93a7f8d2
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-29 21:12:19 -0300

    feat:add axios

--- app/FrontEnd/Views/iot/src/app/users/page.tsx
+++ app/FrontEnd/Views/iot/src/app/users/page.tsx
@@ -18,7 +18,6 @@ export default function Users() {
     setShowDeleteModal(false);
   };
 
-  // Função para buscar usuários
   const fetchUsers = async () => {
     try {
       const response = await axios.get('http://localhost:5257/api/usuario');
