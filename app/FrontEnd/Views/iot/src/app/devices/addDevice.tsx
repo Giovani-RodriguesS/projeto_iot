17319fd2f934012df386ea44f8865954e73291f0
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-12-02 11:08:10 -0300

    feat:add set data

--- app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/addDevice.tsx
@@ -13,14 +13,14 @@ export default function AddDevice() {
                 visible={visible} 
                 onHide={() => setVisible(false)}
                 style={{ top: '10%' }}
-                className="w-full md:w-80 h-4/5 text-center bg-transparent justify-center sidebar-large-text">
+                className="w-full md:w-80 h-4/5 text-center bg-transparent justify-center">
                 <h2 className="sidebar-title mb-2" style={{fontSize:'1.5rem'}}>Cadastro Dispositivo</h2>
                 <SidebarDevices />
             </Sidebar>
             <Button 
                 icon="pi pi-file-plus" 
                 onClick={() => setVisible(true)} 
-                className="p-button-rounded p-button-lg ml-24" 
+                className="p-button-rounded p-button-lg ml-80" 
                 style={{ fontSize: '3rem' }}
             />
         </div>
