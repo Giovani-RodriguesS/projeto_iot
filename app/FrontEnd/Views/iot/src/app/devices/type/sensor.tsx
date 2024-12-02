2f721c8ab3ce3fc3fa6c99a94be60be4a12be01a
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-12-02 11:06:48 -0300

    feat:add sensor

--- app/FrontEnd/Views/iot/src/app/devices/type/sensor.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/type/sensor.tsx
@@ -1,30 +1,40 @@
-import { Button } from "primereact/button";
+import React from "react";
 import { InputText } from "primereact/inputtext";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
+interface SensorProps {
+  formData: {
+    nome: string;
+    data_instalacao: string;
+    localizacao: string;
+  };
+  onFormChange: (key: string, value: string) => void;
+}
 
-export default function Sensor() {
-    return (
-        <div className="card flex flex-col gap-4 p-3 bg-gray-100 rounded-lg">
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-cog"></i>
-                </div>
-                <InputText placeholder="Nome" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
+export default function Sensor({ formData, onFormChange }: SensorProps) {
+  const fields = [
+    { key: "nome", placeholder: "NOME", icon: "user" },
+    { key: "umidade", placeholder: "UMIDADE", icon: "slack" },
+    { key: "data_instalacao", placeholder: "DATA INSTALAÇÃO", icon: "calendar" },
+  ];
 
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-cloud"></i>
-                </div>
-                <InputText placeholder="Valor Umidade" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
-
-            <div className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
-                <div className="text-blue-500 text-2xl mr-4">
-                    <i className="pi pi-calendar"></i>
-                </div>
-                <InputText placeholder="Data Instalação" className="w-full p-2 focus:outline-none" style={{color:'black'}}/>
-            </div>
+  return (
+    <div className="card flex flex-col gap-4 p-6 bg-gray-100 rounded-lg">
+      {fields.map(({ key, placeholder, icon }) => (
+        <div key={key} className="flex items-center border border-gray-300 rounded-lg bg-white p-4">
+          <div className="text-blue-500 text-2xl mr-4">
+            <i className={`pi pi-${icon}`}></i>
+          </div>
+          <InputText
+            placeholder={placeholder}
+            value={formData[key as keyof typeof formData]}
+            onChange={(e) => onFormChange(key, e.target.value)}
+            className="w-full p-2 focus:outline-none"
+            style={{ color: "black" }}
+          />
         </div>
-    );
+      ))}
+    </div>
+  );
 }
