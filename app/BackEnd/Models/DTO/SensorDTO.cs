27226ca2109b59402cfa14c92bbeca1683667b91
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-12-04 17:24:18 -0300

    refactor: models has been updated

--- app/BackEnd/Models/DTO/SensorDTO.cs
+++ app/BackEnd/Models/DTO/SensorDTO.cs
@@ -1,8 +1,8 @@
 public class SensorDTO
 {   
     public string? Nome { get; set; }
-    public float Umidade { get; set; }
     public int NumPino { get; set; }
     public string Tipo { get; set; }
     public DateOnly Data_instalacao { get; set; }
+    public float Umidade { get; set; }
 }
--- app/BackEnd/Models/DTO/UsuarioDTO.cs
+++ app/BackEnd/Models/DTO/UsuarioDTO.cs
@@ -2,7 +2,7 @@ public class UsuarioDTO
 {
     public string? Nome { get; set; }
     public string? Email { get; set; }
-    public string? Senha { get; set; }
     public string? Telefone { get; set; }
+    public string? Senha { get; set; }
     public string? Cargo { get; set; }
 }
