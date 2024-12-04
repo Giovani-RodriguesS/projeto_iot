35a7789cb7f22278183b102aee6621ab5dd5c5c8
Anhaia09 <leticia.anhaia09@gmail.com>
2024-12-04 15:41:40 -0300

    feat: updating answers

--- app/FrontEnd/Views/iot/src/app/devices/page.tsx
+++ app/FrontEnd/Views/iot/src/app/devices/page.tsx
@@ -29,8 +29,8 @@ export default function Users() {
           <div className="flex flex-wrap justify-center gap-5 mt-2">
 
             {/* div do meu funcionário Luciano */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/sensor_umidade.jpg" width={80} height={80} className="mb-3 rounded-full" alt="Luciano" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/sensor_umidade.jpg" width={70} height={70} className="mb-3 rounded-full" alt="Luciano" />
               <div className="text-lg font-bold text-black">Sensor</div>
               <div className="text-sm text-black">ID: 001233</div>
               <div className="text-sm text-black font-bold">Tipo: Umidade</div>
@@ -46,8 +46,8 @@ export default function Users() {
             </div>
 
             {/* div do meu funcionário Roberto */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/sensor_chuva.png" width={80} height={80} className="mb-3 rounded-full" alt="Roberto" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/sensor_chuva.png" width={70} height={70} className="mb-3 rounded-full" alt="Roberto" />
               <div className="text-lg font-bold text-black">Sensor</div>
               <div className="text-sm text-black">ID: 001345</div>
               <div className="text-sm text-black font-bold">Tipo: Chuva</div>
@@ -63,8 +63,8 @@ export default function Users() {
             </div>
 
             {/* div da minha funcionária Bruna */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/sensor_umidade.jpg" width={80} height={80} className="mb-3 rounded-full" alt="Bruna" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/sensor_umidade.jpg" width={70} height={70} className="mb-3 rounded-full" alt="Bruna" />
               <div className="text-lg font-bold text-black">Sensor</div>
               <div className="text-sm text-black">ID: 003212</div>
               <div className="text-sm text-black font-bold">Tipo: Umidade</div>
@@ -81,8 +81,8 @@ export default function Users() {
 
 
             {/* div da minha funcionária Giseli */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/sensor_chuva.png" width={80} height={80} className="mb-3 rounded-full" alt="Giseli" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/sensor_chuva.png" width={70} height={70} className="mb-3 rounded-full" alt="Giseli" />
               <div className="text-lg font-bold text-black">Sensor</div>
               <div className="text-sm text-black">ID: 009245</div>
               <div className="text-sm text-black font-bold">Tipo: Chuva</div>
@@ -98,8 +98,8 @@ export default function Users() {
             </div>
 
             {/* div da minha funcionária Luana */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/sensor_chuva.png" width={80} height={80} className="mb-3 rounded-full" alt="Luana" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/sensor_chuva.png" width={70} height={70} className="mb-3 rounded-full" alt="Luana" />
               <div className="text-lg font-bold text-black">Sensor</div>
               <div className="text-sm text-black">ID: 000298</div>
               <div className="text-sm text-black font-bold">Tipo: Chuva</div>
@@ -115,8 +115,8 @@ export default function Users() {
             </div>
 
             {/* div do meu funcionário Lucas */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/Bomba.webp" width={80} height={80} className="mb-3 rounded-full" alt="Lucas" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/Bomba.webp" width={70} height={70} className="mb-3 rounded-full" alt="Lucas" />
               <div className="text-lg font-bold text-black">Bomba</div>
               <div className="text-sm text-black">ID: 011298</div>
               <div className="text-sm text-black font-bold">Tipo: Escova</div>
@@ -132,8 +132,8 @@ export default function Users() {
             </div>
 
             {/* div da minha funcionária Letícia */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/Bomba.webp" width={80} height={80} className="mb-3 rounded-full" alt="Letícia" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/Bomba.webp" width={70} height={70} className="mb-3 rounded-full" alt="Letícia" />
               <div className="text-lg font-bold text-black">Bomba</div>
               <div className="text-sm text-black">ID: 011223</div>
               <div className="text-sm text-black font-bold">Tipo: Escova</div>
@@ -149,8 +149,8 @@ export default function Users() {
             </div>
 
             {/* div do meu funcionário Gabriel */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/Bomba.webp" width={80} height={80} className="mb-3 rounded-full" alt="Gabriel" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/Bomba.webp" width={70} height={70} className="mb-3 rounded-full" alt="Gabriel" />
               <div className="text-lg font-bold text-black">Bomba</div>
               <div className="text-sm text-black">ID: 011234</div>
               <div className="text-sm text-black font-bold">Tipo: Escova</div>
@@ -166,8 +166,8 @@ export default function Users() {
             </div>
 
             {/* div do meu funcionário Rafael */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/Bomba.webp" width={80} height={80} className="mb-3 rounded-full" alt="Rafael" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/Bomba.webp" width={70} height={70} className="mb-3 rounded-full" alt="Rafael" />
               <div className="text-lg font-bold text-black">Bomba</div>
               <div className="text-sm text-black">ID: 098843</div>
               <div className="text-sm text-black font-bold">Tipo: Escova</div>
@@ -183,8 +183,8 @@ export default function Users() {
             </div>
 
             {/* div da minha funcionária Aline */}
-            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-48 flex flex-col items-center">
-              <Image src="/images/devices/Bomba.webp" width={80} height={80} className="mb-3 rounded-full" alt="Aline" />
+            <div className="p-4 bg-gray-200 dark:bg-white rounded-xl w-30 flex flex-col items-center">
+              <Image src="/images/devices/Bomba.webp" width={70} height={70} className="mb-3 rounded-full" alt="Aline" />
               <div className="text-lg font-bold text-black">Bomba</div>
               <div className="text-sm text-black">ID: 014598</div>
               <div className="text-sm text-black font-bold">Tipo: Escova</div>
