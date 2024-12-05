8a49bef0b783570278805621a370c7bb2d6a83fc
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-12-04 22:18:25 -0300

    refactor: models has been updated

--- app/BackEnd/Models/DTO/BombaDTO.cs
+++ app/BackEnd/Models/DTO/BombaDTO.cs
@@ -3,7 +3,7 @@ public class BombaDTO
     public string? Nome { get; set; }
     public int NumPino { get; set; }
     public string? Tipo { get; set; }
-    public DateOnly Data_instalacao { get; set; }
+    public string Data_instalacao { get; set; }
     public float Vazao { get; set; }
     public string? Localizacao { get; set; }
 }
