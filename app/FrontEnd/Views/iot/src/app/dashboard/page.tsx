7a8308f35bb73ebdbe3f02eda2a714c99b337226
Anhaia09 <leticia.anhaia09@gmail.com>
2024-12-11 20:29:21 -0300

    feat: updated dashboard cards

--- app/FrontEnd/Views/iot/src/app/dashboard/page.tsx
+++ app/FrontEnd/Views/iot/src/app/dashboard/page.tsx
@@ -2,9 +2,36 @@ import React from "react";
 import LineChart from "@/app/dashboard/LineChart";
 import PieChart from "@/app/dashboard/PieChart";
 import Header from "@/components/Header";
+import axios from "axios";
 
 
-export default function Reports() {
+export default async function Reports() {
+  
+  const sensorResponse = await axios.get('http://localhost:80/api/LeituraSensor');
+  const bombaResponse = await axios.get('http://localhost:80/api/LeituraBomba');
+
+  const sensorData = sensorResponse.data
+
+  const leiturasDeChuva = sensorData.filter((item: { idSensor: number; }) => item.idSensor === 2);
+  const leiturasUmidade = sensorData.filter((item: { idSensor: number; }) => item.idSensor === 1);
+
+  const ultimaMedidaUmidade = leiturasUmidade[leiturasUmidade.length - 1]?.medida;
+  const ultimaMedidaChuva = leiturasDeChuva[leiturasDeChuva.length - 1]?.medida;
+
+  const bombaData = bombaResponse.data
+
+  const ultimoEstadoBomba = bombaData[bombaData.length - 1]?.BombaAtivada
+  
+  const getBombaStatus = () => {
+    if (ultimoEstadoBomba === 0) {
+      return "Bomba Ativada";
+    } else {
+      return "Bomba Desativada";
+    }
+  };
+
+  const bombaStatus = getBombaStatus();
+
   return (
     <div className="flex min-h-screen overflow-x-hidden">
       <div className="flex-1 bg-white dark:bg-slate-800 text-black dark:text-white w-full">
@@ -14,23 +41,23 @@ export default function Reports() {
             <div className="p-4 bg-red-500 rounded-lg w-60 flex flex-col items-center justify-center">
               <div>
                 <i className="pi pi-clock text-2xl px-2"></i>
-                <span className="text-2xl">13</span>
+                <span className="text-2xl">{ultimaMedidaUmidade}</span>
               </div>
-              <div className="text-lg pt-2">Detecção de choveu</div>
+              <div className="text-lg pt-2">% Umidade atual</div>
             </div>
             <div className="p-4 bg-orange-500 rounded-lg w-60 flex flex-col items-center justify-center">
               <div>
                 <i className="pi pi-cog text-2xl px-2"></i>
-                <span className="text-2xl">10min</span>
+                <span className="text-2xl">{bombaStatus}</span>
               </div>
-              <div className="text-lg pt-2">Tempo Bomba Ligada</div>
+              <div className="text-lg pt-2">Estado da bomba</div>
             </div>
             <div className="p-4 bg-green-500 rounded-lg w-60 flex flex-col items-center justify-center">
               <div>
                 <i className="pi pi-exclamation-triangle text-2xl px-2"></i>
-                <span className="text-2xl">1000ml</span>
+                <span className="text-2xl">{ultimaMedidaChuva}</span>
               </div>
-              <div className="text-lg pt-2">Água Economizada</div>
+              <div className="text-lg pt-2">% Umidade Chuva Atual</div>
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8 w-full px-4">
