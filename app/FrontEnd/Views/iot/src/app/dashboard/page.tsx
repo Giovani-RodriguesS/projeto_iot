344304085a4ead5ba7329a65dd300a40d4d4048f
Anhaia09 <leticia.anhaia09@gmail.com>
2024-12-11 22:15:52 -0300

    feat: updated the icons and colors of the dashboard screen cards

--- app/FrontEnd/Views/iot/src/app/dashboard/page.tsx
+++ app/FrontEnd/Views/iot/src/app/dashboard/page.tsx
@@ -3,10 +3,13 @@ import LineChart from "@/app/dashboard/LineChart";
 import PieChart from "@/app/dashboard/PieChart";
 import Header from "@/components/Header";
 import axios from "axios";
+import { FaToggleOff } from "react-icons/fa6";
+import { FaCloudRain } from "react-icons/fa";
+import { FaDroplet } from "react-icons/fa6";
 
 
 export default async function Reports() {
-  
+
   const sensorResponse = await axios.get('http://localhost:80/api/LeituraSensor');
   const bombaResponse = await axios.get('http://localhost:80/api/LeituraBomba');
 
@@ -21,12 +24,12 @@ export default async function Reports() {
   const bombaData = bombaResponse.data
 
   const ultimoEstadoBomba = bombaData[bombaData.length - 1]?.BombaAtivada
-  
+
   const getBombaStatus = () => {
     if (ultimoEstadoBomba === 0) {
-      return "Bomba Ativada";
+      return "Ativada";
     } else {
-      return "Bomba Desativada";
+      return "Desativada";
     }
   };
 
@@ -37,37 +40,46 @@ export default async function Reports() {
       <div className="flex-1 bg-white dark:bg-slate-800 text-black dark:text-white w-full">
         <Header title="Irrigação Smart" username="Usuário" />
         <main>
-          <div className="flex flex-wrap gap-4 justify-center mt-7 px-5">
-            <div className="p-4 bg-red-500 rounded-lg w-60 flex flex-col items-center justify-center">
-              <div>
-                <i className="pi pi-clock text-2xl px-2"></i>
+          <div className="flex flex-wrap gap-8 justify-center mt-7 px-5">
+
+            {/* Card Umidade */}
+            <div className="p-4 bg-blue-200 rounded-lg w-80 flex flex-col items-center justify-center text-black dark:text-black">
+              <div className="flex items-center">
+                <i className="text-3xl mr-2"><FaDroplet /></i>
                 <span className="text-2xl">{ultimaMedidaUmidade}</span>
               </div>
-              <div className="text-lg pt-2">% Umidade atual</div>
+              <div className="text-lg pt-2">Umidade Atual</div>
             </div>
-            <div className="p-4 bg-orange-500 rounded-lg w-60 flex flex-col items-center justify-center">
-              <div>
-                <i className="pi pi-cog text-2xl px-2"></i>
+
+            {/* Card Bomba */}
+            <div className="p-4 bg-red-500 rounded-lg w-80 flex flex-col items-center justify-center text-black dark:text-black">
+              <div className="flex items-center">
+                <i className="text-4xl mr-2"><FaToggleOff /></i>
                 <span className="text-2xl">{bombaStatus}</span>
               </div>
-              <div className="text-lg pt-2">Estado da bomba</div>
+              <div className="text-lg pt-2">Estado da Bomba</div>
             </div>
-            <div className="p-4 bg-green-500 rounded-lg w-60 flex flex-col items-center justify-center">
-              <div>
-                <i className="pi pi-exclamation-triangle text-2xl px-2"></i>
+
+            {/* Card Umidade Chuva */}
+            <div className="p-4 bg-blue-400 rounded-lg w-80 flex flex-col items-center justify-center text-black dark:text-black">
+              <div className="flex items-center">
+                <i className="text-4xl mr-2"><FaCloudRain /></i>
                 <span className="text-2xl">{ultimaMedidaChuva}</span>
               </div>
-              <div className="text-lg pt-2">% Umidade Chuva Atual</div>
+              <div className="text-lg pt-2">Umidade de Chuva Atual</div>
             </div>
+
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8 w-full px-4">
+
+
+          {/* Gráficos*/}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-9 w-full px-4">
             <div className="bg-gray-200 dark:bg-white p-2 rounded-lg" style={{ overflow: 'hidden' }}>
               <h2 className="text-black text-xl md:text-2xl text-center">Monitoramento Sensores</h2>
               <div className="mt-6 w-full h-full">
                 <LineChart />
               </div>
             </div>
-
 
             <div className="bg-gray-200 dark:bg-white p-2 rounded-lg flex flex-col items-center" style={{ height: '400px', overflow: 'hidden' }}>
               <h2 className="text-black text-xl md:text-2xl mb-4">Monitoramento Chuva</h2>
